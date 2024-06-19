706c9b3197b7cc39f4097daaeb3a6ad2317433cb
mrtwumgh <twumstephen2006@gmail.com>
2024-06-19 07:43:08 +0000

    simple_interest.py script added

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+principal = 1000 # Principal amount of $1000
+rate = 0.05 # annual interest rate of 5%
+time = 3 # represents 3 years
+
+simple_interest = principal * rate * time
+print("The simple interest is: {}".format(simple_interest))
