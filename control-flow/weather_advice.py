2f33fdbfceabff202f395f5ffeb237262f410de3
mrtwumgh <twumstephen2006@gmail.com>
2024-06-26 08:20:13 +0000

    modified: weather_advice.py included lower() methods for the weather_input variable

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
 weather_input = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if weather_input == "sunny":
+if weather_input.lower() == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather_input == "rainy":
+elif weather_input.lower() == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather_input == "cold":
+elif weather_input.lower() == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
