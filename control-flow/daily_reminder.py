98dea9e2123a3725fcae370c402989f5a04d27e6
mrtwumgh <twumstephen2006@gmail.com>
2024-06-27 06:49:02 +0000

    modified: removed the loop and put the print statements in the match case statements

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,24 +6,22 @@ reminder = ""
 match priority:
     case "high":
         if time_bound == "yes":
-            reminder = "ht"
+            reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
+            print(f"Reminder: {reminder}")
         elif time_bound == "no":
-            reminder = "hnt"
+            reminder = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
+            print(f"Note: {reminder}")
     case "medium":
         if time_bound == "yes":
-            reminder = "mt"
+            reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
+            print(f"Reminder: {reminder}")
         elif time_bound == "no":
-            reminder = "mnt"
+            reminder = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
+            print(f"Note: {reminder}")
     case "low":
         if time_bound == "yes":
-            reminder = "lt"
+            reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
+            print(f"Reminder: {reminder}")
         elif time_bound == "no":
-            reminder = "lnt"
-print()
-while True:
-    if reminder == "ht" or "mt" or "lt":
-        print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
-        break
-    elif reminder == "hnt" or "mnt" or "lnt":
-        print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
-        break
+            reminder = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
+            print(f"Note: {reminder}")
