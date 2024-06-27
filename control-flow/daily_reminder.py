934781660927cde1155cbdd0d74591fb36d3541d
mrtwumgh <twumstephen2006@gmail.com>
2024-06-27 08:37:12 +0000

    modified: changed logic to use on two print statements

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,21 +4,7 @@ time_bound = input("Is it time-bound? (yes/no): ")
 reminder = ""
 
 match priority:
-    case "high":
-        if time_bound == "yes":
-            reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
-            print("Reminder:", reminder)
-        elif time_bound == "no":
-            reminder = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
-            print("Note:", reminder)
-    case "medium":
-        if time_bound == "yes":
-            reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
-            print("Reminder:", reminder)
-        elif time_bound == "no":
-            reminder = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
-            print("Note:", reminder)
-    case "low":
+    case "high" | "medium" | "low":
         if time_bound == "yes":
             reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
             print("Reminder:", reminder)
