5ff569457881d6231074952703cc3c8cc31ae616
mrtwumgh <twumstephen2006@gmail.com>
2024-06-27 07:00:26 +0000

    modified: used an outer if else statement to print the output

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,21 +7,20 @@ match priority:
     case "high":
         if time_bound == "yes":
             reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
-            print(f"Reminder: {reminder}")
         elif time_bound == "no":
             reminder = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
-            print(f"Note: {reminder}")
     case "medium":
         if time_bound == "yes":
             reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
-            print(f"Reminder: {reminder}")
         elif time_bound == "no":
             reminder = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
-            print(f"Note: {reminder}")
     case "low":
         if time_bound == "yes":
             reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
-            print(f"Reminder: {reminder}")
         elif time_bound == "no":
             reminder = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
-            print(f"Note: {reminder}")
+
+if time_bound == "yes":
+    print(f"Reminder: {reminder}")
+elif time_bound == "no":
+    print(f"Note: {reminder}")
