e5c6b045a814df96d1c035869bf4140145684c9c
mrtwumgh <twumstephen2006@gmail.com>
2024-06-28 06:27:54 +0000

    modified: separated the case statements

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,7 +4,21 @@ time_bound = input("Is it time-bound? (yes/no): ")
 reminder = ""
 
 match priority:
-    case "high" | "medium" | "low":
+    case "high":
+        if time_bound == "yes":
+            reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
+            print("Reminder:", reminder)
+        elif time_bound == "no":
+            reminder = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
+            print("Note:", reminder)
+    case "medium":
+        if time_bound == "yes":
+            reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
+            print("Reminder:", reminder)
+        elif time_bound == "no":
+            reminder = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
+            print("Note:", reminder)
+    case "low":
         if time_bound == "yes":
             reminder = f"'{task}' is a {priority} priority task that requires immediate attention today!"
             print("Reminder:", reminder)
