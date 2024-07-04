88863b53ad4f8ac7f9dc9cc4897bcfb99360ca65
mrtwumgh <twumstephen2006@gmail.com>
2024-07-04 08:56:36 +0000

    modified: formatted the object current_time using strftime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,8 +1,9 @@
 from datetime import datetime, date, timedelta
 
 def display_current_datetime():
-    current_time = datetime.now().replace(microsecond=0)
-    print(f"Current date and time: {current_time}")
+    current_time = datetime.now()
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {formatted_time}")
 
 def calculate_future_date(number):
     tday = date.today()
