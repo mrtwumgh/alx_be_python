23175be4f49e45d736f3375699b6a8478b0a24e4
mrtwumgh <twumstephen2006@gmail.com>
2024-07-04 07:38:51 +0000

    modified: changed the prompts for add and remove

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,11 +13,11 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item_name = input("Enter an item to add the shopping list: ")
+            item_name = input("Enter the item to add: ")
             shopping_list.append(item_name)
         elif choice == '2':
             # Prompt for and remove an item
-            item_name = input("Enter an item to remove from the shopping list: ")
+            item_name = input("Enter the item to remove: ")
             shopping_list.remove(item_name)
         elif choice == '3':
             # Display the shopping list
