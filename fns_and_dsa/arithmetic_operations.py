549097ecb933617594daf07399e8e88b80a9a847
mrtwumgh <twumstephen2006@gmail.com>
2024-07-04 07:29:29 +0000

    modified: changed logic from match case to if else

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,16 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            result = num1 + num2
+    if operation == "add":
+        result = num1 + num2
+        return result
+    elif operation == "subtract":
+        result = num1 - num2
+        return result
+    elif operation == "multiply":
+        result = num1 * num2
+        return result
+    elif operation == "divide":
+        if num2 == 0:
+            return "Cannot divide by zero"
+        else:
+            result = num1 / num2
             return result
-        case "subtract":
-            result = num1 - num2
-            return result
-        case "multiply":
-            result = num1 * num2
-            return result
-        case "divide":
-            if num2 == 0:
-                return "Cannot divide by zero"
-            else:
-                result = num1 / num2
-                return result
