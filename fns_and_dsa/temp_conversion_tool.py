a637f076d588a25540e703b4329b70ac9c624197
mrtwumgh <twumstephen2006@gmail.com>
2024-07-05 05:44:12 +0000

    modified: add input validation

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,21 +1,24 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-def convert_to_celsius(fahrenheit):
-    celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
-    return celsius
-
 def convert_to_fahrenheit(celsius):
     fahrenheit = CELSIUS_TO_FAHRENHEIT_FACTOR * celsius + 32
     return fahrenheit
 
-temperature = int(input("Enter the temperature to convert: "))
+def convert_to_celsius(fahrenheit):
+    celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
+    return celsius
+
+temperature = input("Enter the temperature to convert: ")
 unit = input("Is this temperature in Celcius or Fahrenheit? (C/F): ")
 
-if unit == "F":
+if temperature.isdigit() and unit.lower() == "f":
+    temperature = int(temperature)
     celcius = convert_to_celsius(temperature)
     print(f"{temperature}°F is {celcius}°C")
-
-if unit == "C":
+elif temperature.isdigit() and unit.lower() == "c":
+    temperature = int(temperature)
     fahrenheit = convert_to_fahrenheit(temperature)
     print(f"{temperature}°C is {fahrenheit}°F")
+else:
+    print("Invalid temperature. Please enter a numeric value.")
